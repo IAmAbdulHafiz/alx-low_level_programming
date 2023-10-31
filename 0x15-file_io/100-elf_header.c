5a9fb07c1d189f30a5769807645d0857c875d23c
IAmAbdulHafiz <abdulhafiz.yussif@aiesec.net>
2023-10-30 23:35:00 -0700

    Fixed Betty style error in codes

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -43,8 +43,7 @@ int main(int argc, char *argv[])
 
 	if (read(fd, &elf_header, sizeof(Elf64_Ehdr)) != sizeof(Elf64_Ehdr))
 	{
-		fprintf(stderr, "Error: Failed to read ELF header from file: %s\n",
-		strerror(errno));
+		fprintf(stderr, "Error: Failed to read ELF header from file: %s\n", strerror(errno));
 		close(fd);
 		return (98);
 	}
@@ -88,19 +87,16 @@ void print_elf_header(const Elf64_Ehdr *header)
 	printf("\n");
 
 	class_str = (header->e_ident[EI_CLASS] == ELFCLASS64) ? "ELF64" : "ELF32";
-	data_str = (header->e_ident[EI_DATA] == ELFDATA2LSB) ? "2's complement,
-	little endian" : "2's complement, big endian";
+	data_str = (header->e_ident[EI_DATA] == ELFDATA2LSB) ? "2's complement,little endian" : "2's complement, big endian";
 
 	printf("  Class:                             %s\n", class_str);
 	printf("  Data:                              %s\n", data_str);
-	printf("  Version:                           %d (current)\n",
-	header->e_ident[EI_VERSION]);
+	printf("  Version:    %d (current)\n", header->e_ident[EI_VERSION]);
 
 	os_abi = (header->e_ident[EI_OSABI] == ELFOSABI_SYSV) ?
 	"UNIX - System V" : "Other";
 	printf("  OS/ABI:                            %s\n", os_abi);
-	printf("  ABI Version:                       %d\n",
-	header->e_ident[EI_ABIVERSION]);
+	printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
 
 	type_str = (header->e_type == ET_EXEC) ? "EXEC (Executable file)" : "Other";
 	printf("  Type:                              %s\n", type_str);
